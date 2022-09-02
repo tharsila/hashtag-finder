dbe2cd9cdc2fe489c862c46449d8f9f4243f95f5
tharsila <tharsilapaula@gmail.com>
2022-09-01 21:39:12 -0300

    feat: header

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, ChangeEvent, MouseEvent } from 'react'
+import Header from '../../components/Header'
+import {Button} from '../../components/Button'
 import * as C from './styles'
 
 interface InfoTwitter {
@@ -108,42 +110,60 @@ const PostList = () => {
   }
  
   return (
-    <C.PostContainer showImage={showImage} showTweet={showTweet}>
-      <h3>Exibindo os 10 resultados mais recentes para #natureza</h3>
-      <div className="choose">
-        <span onClick={changeLink}>Tweets</span>
-        <span onClick={changeLink}>Imagens</span>
-      </div>
-      <C.PostContent>
-        <C.PostGrid showImage={showImage}>
-          {info.map((item, index) => (
-            <C.PostImg key={index}>
-              <img src={item.postImg} alt="" />
-              <C.TwitterInfo>
-                <p>Postado por:</p>
-                <strong>{item.accountName}</strong>
-              </C.TwitterInfo>
-            </C.PostImg>
-          ))}
-        </C.PostGrid>
-        
-        <C.PostCardContainer showTweet={showTweet}>
-          {info.map((item, index) => (
-            <C.PostCard key={index}>
-              <div className='userPhoto'>
-                <img src={item.userPhoto} alt="" />
-              </div>
-              <div className='userInfo'>
-                <strong>{item.userName}</strong>
-                <span>{item.accountName}</span>
-                <p>{item.content}</p>
-                <a href="">Ver mais no Twitter</a>
-              </div>
-            </C.PostCard>
-          ))}
-        </C.PostCardContainer>
-      </C.PostContent>
-    </C.PostContainer>
+    <>
+      <Header>
+        <div>
+          <Button
+              icon="icon-info-circle.svg"
+              text="SOBRE"
+              color="#0B1741"
+              backgroundColor="#72EFDB"
+          />
+          <Button
+              icon="icon-user-alt.svg"
+              text="LOGIN"
+              color="#FFFFFF"
+              backgroundColor="#1E3E7B"
+          />
+        </div>
+      </Header>
+      <C.PostContainer showImage={showImage} showTweet={showTweet}>
+        <h3>Exibindo os 10 resultados mais recentes para #natureza</h3>
+        <div className="choose">
+          <span onClick={changeLink}>Tweets</span>
+          <span onClick={changeLink}>Imagens</span>
+        </div>
+        <C.PostContent>
+          <C.PostGrid showImage={showImage}>
+            {info.map((item, index) => (
+              <C.PostImg key={index}>
+                <img src={item.postImg} alt="" />
+                <C.TwitterInfo>
+                  <p>Postado por:</p>
+                  <strong>{item.accountName}</strong>
+                </C.TwitterInfo>
+              </C.PostImg>
+            ))}
+          </C.PostGrid>
+          
+          <C.PostCardContainer showTweet={showTweet}>
+            {info.map((item, index) => (
+              <C.PostCard key={index}>
+                <div className='userPhoto'>
+                  <img src={item.userPhoto} alt="" />
+                </div>
+                <div className='userInfo'>
+                  <strong>{item.userName}</strong>
+                  <span>{item.accountName}</span>
+                  <p>{item.content}</p>
+                  <a href="">Ver mais no Twitter</a>
+                </div>
+              </C.PostCard>
+            ))}
+          </C.PostCardContainer>
+        </C.PostContent>
+      </C.PostContainer>
+    </>
   )
 }
 
