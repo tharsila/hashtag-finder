cb9af2ce2b82ff86c665158fb235d78c95ffc5b5
Kaique <kaique_gp@hotmail.com>
2022-09-02 23:02:25 -0300

    resolving conflit

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import LoginUser from "../../components/login/bodyLogin/body";
+import { BodyLogin } from "../../components/login/bodyLogin/styles";
 import Header from "../../components/login/headerLogin/headerLogin"
 export default function Login () {
     return(
-        <div>
+        <BodyLogin>
             <Header/>
             <LoginUser/>
-        </div>
+        </BodyLogin>
     );
 }
