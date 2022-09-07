63d3235f35286c8b23cdf4a8d06e6880bdd7a18c
arthur Borges <arthurborges@ufu.br>
2022-09-07 01:54:44 -0300

    resolving conflicts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,11 @@
 import * as C from "./styles";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 export const Header = ({ children }: { children: JSX.Element[] }) => {
     return (
         <C.Container>
             <C.Header>
-<<<<<<< HEAD
                 <C.Logo>
                     <span>hashtag</span>
                     <span id="strong">finder</span>
@@ -14,15 +13,6 @@ export const Header = ({ children }: { children: JSX.Element[] }) => {
                 <div>
                     {children}
                 </div>
-=======
-                <Link to = '/'>
-                    <C.Logo>
-                        <span>hashtag</span>
-                        <span id="strong">finder</span>
-                    </C.Logo>
-                </Link>
-                {children}
->>>>>>> main
             </C.Header>
         </C.Container>
     );
