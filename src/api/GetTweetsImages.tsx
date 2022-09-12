38d5a03f7926d961b83b8ffe47218828a409026f
tharsila <tharsilapaula@gmail.com>
2022-09-12 04:39:56 -0300

    feat: api twitter

---
+++ src/api/GetTweetsImages.tsx
@@ -0,0 +1,23 @@
+
+const getTweetImages = async (searchValue:string, moreRequest:number) => {
+ 
+  const url = `https://cors.eu.org/https://api.twitter.com/2/tweets/search/recent?query=${searchValue}%20has:hashtags%20-is:retweet%20-is:quote%20has:images&max_results=${moreRequest}&expansions=author_id,attachments.media_keys&user.fields=id,name,username,profile_image_url,url&media.fields=type,url,width,height&tweet.fields=source`;
+
+  const token =
+    'AAAAAAAAAAAAAAAAAAAAAFlKHgEAAAAApBW4nRyRkiogluzAbXlS4KuHlMU%3DFcR7r8N19LRnMHLVmYlFsod6Be6zUvZD2rxATotl6mLPAh2UEX';
+
+  const myHeaders = new Headers();
+  myHeaders.append('Authorization', `Bearer ${token}`);
+
+  const requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+  };
+
+  const response = await fetch(url, requestOptions);
+  const body = await response.json();
+
+  return body;
+};
+
+export { getTweetImages };
