2e05110cc1427c71f32f1b100a5cb39357accc65
tharsila <tharsilapaula@gmail.com>
2022-09-19 21:31:04 -0300

    fix: route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,7 +7,7 @@ import {
 
 
 import Login from "./pages/login/login";
-import PostList from "./pages/home";
+import Home from "./pages/home";
 import { About } from "./pages/about";
 import { SearchedHashtags } from "./pages/SearchedHashtags";
 import { RequireAuth } from "./contexts/Auth/RequireAuth";
@@ -30,7 +30,7 @@ const AppRoutes = () => {
                             <SearchedHashtags/>
                         </RequireAuth>
                     } />
-                    <Route path="/" element={<PostList />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/About" element={<About />} />
                 </Routes>
             </AuthProvider>
